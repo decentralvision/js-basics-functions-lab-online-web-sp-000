536bb3fdd047c9318c42d399da4c18b86eacf768
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-18 15:29:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 // Code your solution in this file!
+function distanceInFeet(blocks) {
+  blocks * 264
+}
 function distanceFromHqInBlocks (someValue) {
   return Math.abs(42 - someValue)
 }
 
 function distanceFromHqInFeet (someValue) {
-  return distanceFromHqInBlocks(someValue) * 264
+  return distanceInFeet(distanceFromHqInBlocks(someValue))
 }
 
 function distanceTravelledInFeet (origin, destination) {
-  return Math.abs(origin - destination)
+  return distanceInFeet(Math.abs(origin - destination))
 }
