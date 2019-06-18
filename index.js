79f12bdd621f960d522df915f598335d129eb057
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-18 15:28:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function distanceFromHqInFeet (someValue) {
   return distanceFromHqInBlocks(someValue) * 264
 }
 
-function distanceTravelledInFeet ()
+function distanceTravelledInFeet (origin, destination) {
+  return Math.abs(origin - destination)
+}
