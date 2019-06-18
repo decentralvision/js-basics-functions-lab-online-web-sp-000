e18f9c4482569e68a8be17e45b3b8f4472f2344b
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-06-18 15:12:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks (someValue) {
-  return (42 - someValue )
+  return Math.abs(42 - someValue )
 }
 
 function distanceFromHqInFeet (someValue) {
